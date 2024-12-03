78bfa96263dc7433853a81a609101768e6fe5109
pu0238 <pu0238@protonmail.com>
2024-12-03 11:58:13 +0000

    Added rates table

--- src/my_project_frontend/src/App.vue
+++ src/my_project_frontend/src/App.vue
@@ -1,28 +1,29 @@
 <script setup>
 import { ref } from 'vue';
-import { my_project_backend } from 'declarations/my_project_backend/index';
-let greeting = ref('');
+// https://api.nbp.pl/api/exchangerates/tables/A/?format=json
 
-async function handleSubmit(e) {
-  e.preventDefault();
-  const target = e.target;
-  const name = target.querySelector('#name').value;
-  await my_project_backend.greet(name).then((response) => {
-    greeting.value = response;
-  });
-}
+const rates = ref([])
+fetch("https://api.nbp.pl/api/exchangerates/tables/A/?format=json").then(async (res) => {
+  const jsonData = await res.json()
+  console.log(jsonData)
+
+  rates.value = jsonData[0].rates
+})
 </script>
 
 <template>
   <main>
-    <img src="/logo2.svg" alt="DFINITY logo" />
-    <br />
-    <br />
-    <form action="#" @submit="handleSubmit">
-      <label for="name">Enter your name: &nbsp;</label>
-      <input id="name" alt="Name" type="text" />
-      <button type="submit">Click Me!</button>
-    </form>
-    <section id="greeting">{{ greeting }}</section>
+  <table>
+    <tr>
+      <th>Kod</th>
+      <th>Nazwa waluty</th>
+      <th>Kurs</th>
+    </tr>
+    <tr v-for="rate in rates">
+      <td>{{  rate.code }}</td>
+      <td>{{  rate.currency }}</td>
+      <td>{{  rate.mid }}</td>
+    </tr>
+  </table>
   </main>
 </template>
